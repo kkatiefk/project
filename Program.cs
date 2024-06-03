9ef40ff0db54075c28e5b8473d8f523ee29f6585
Kateryna Verhun <vergun.katerina@lll.kpi.ua>
2024-06-03 11:23:34 +0300

    знову

--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,9 +18,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
