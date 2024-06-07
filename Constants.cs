17494e4bc78a7ca65c241fa52ae380f57f970278
Kateryna Verhun <161355341+kkatiefk@users.noreply.github.com>
2024-06-07 22:57:58 +0300

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public static string Address = "https://imdb146.p.rapidapi.com/v1/find/?query=brad";
-        public static string ApiKey = "0bb6deb783msh00370c5b0161dfdp178515jsn2fdfd5478dfc";
+        public static string ApiKey = "MyApiKey";
         public static string ApiHost = "imdb146.p.rapidapi.com";
         public static string Connect = "Host=localhost;Username=postgres;Password=1524;Database=ProjectTelegram";
     }
